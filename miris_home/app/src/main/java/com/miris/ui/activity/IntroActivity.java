05cd48f02779d8668f0bea89b3600b3316e9c089
miris-home <miris@miris.com>
2015-09-02 17:54:30 +0900

    [Add] introActivity
    Signed-off-by: miris-home <miris@miris.com>

---
+++ miris_home/app/src/main/java/com/miris/ui/activity/IntroActivity.java
@@ -0,0 +1,18 @@
+package com.miris.ui.activity;
+
+import android.os.Bundle;
+
+import com.miris.R;
+
+/**
+ * Created by Miris on 09.02.15.
+ */
+public class IntroActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+    }
+}
