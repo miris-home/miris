23331bea8b602ac5be279173191afd6e48b43737
miris-home <plus334@naver.com>
2016-07-28 11:18:19 +0900

    쪽지 기능 추가

---
+++ miris_home/app/src/main/java/com/miris/ui/activity/MessageSendActivity.java
@@ -0,0 +1,16 @@
+package com.miris.ui.activity;
+
+import android.os.Bundle;
+
+import com.miris.R;
+
+/**
+ * Created by miris on 2016-07-22.
+ */
+public class MessageSendActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_detail);
+    }
+}
